8796f19672653750fa74ac56af0958ee4c79cc4a
Wynter Woods <zerotri@gmail.com>
2016-12-21 15:54:12 -0800

    Add blinkenlights demo defconfig and initscript

--- gadget/package/demo-blinkenlights/demo-blinkenlights.mk
+++ gadget/package/demo-blinkenlights/demo-blinkenlights.mk
@@ -13,6 +13,7 @@ DEMO_BLINKENLIGHTS_LICENSE = MIT
 ####
 
 define DEMO_BLINKENLIGHTS_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 755 $(@D)/S15_blinkenlights $(TARGET_DIR)/etc/init.d/
 	$(INSTALL) -D -m 755 $(@D)/blink-leds $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -D -m 755 $(@D)/fade-pwms $(TARGET_DIR)/usr/bin/
 endef
