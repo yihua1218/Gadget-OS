c4d87a932c508589fed1641b9cd4706172d9a00e
Wynter Woods <zerotri@gmail.com>
2017-02-09 15:19:05 -0800

    Too many changes in this commit.   - Added CHIP support. Still buggy.   - Added CHIP-MTD-utils package for MLC support.   - Modified chippro_docker_defconfig.     - Removed python, demo-vu-meter, chip_io.     - Added wget, nano.

--- gadget/board/nextthing/chip/scripts/post-image.sh
+++ gadget/board/nextthing/chip/scripts/post-image.sh
@@ -12,7 +12,6 @@ BOARD_DIR=${ROOT_DIR}/board/nextthing/chippro
 IMAGE_TYPE="${2}"
 if [ -n "${IMAGE_TYPE}" ]; then
 	IMAGE_CONFIG_FILE="${BOARD_DIR}/configs/nand/${IMAGE_TYPE}.config"
-	echo "Image type: ${IMAGE_TYPE}"
 
 	if [ ! -f "${IMAGE_CONFIG_FILE}" ]; then
 		echo -e "Selected NAND type: ${IMAGE_TYPE} but config file does not exist:"
@@ -20,7 +19,6 @@ if [ -n "${IMAGE_TYPE}" ]; then
 		exit 1
 	fi
 
-	echo "Loading config file: ${IMAGE_CONFIG_FILE}"
 	source "${IMAGE_CONFIG_FILE}"
 
 	IMG2SIMG=$(which img2simg)
@@ -29,12 +27,11 @@ if [ -n "${IMAGE_TYPE}" ]; then
 		echo -e "\tThis is likely located inside of the 'android-tools-fsutils' package"
 		exit 1
 	else
-		echo -e "Writing sparse ubi image for nand type: ${IMAGE_TYPE}"
+		echo -e "Writing sparse ubi image for nand type: ${IMAGE_CONFIG}"
 		echo -e "\terase block size: ${NAND_ERASE_BLOCK_SIZE}"
 		${IMG2SIMG} ${BINARIES_DIR}/rootfs.ubi ${BINARIES_DIR}/rootfs.ubi.sparse "${NAND_ERASE_BLOCK_SIZE}"
 	fi
-
-	NAND_ERASE_BLOCK_SIZE=${NAND_ERASE_BLOCK_SIZE} NAND_PAGE_SIZE=${NAND_PAGE_SIZE} NAND_OOB_SIZE=${NAND_OOB_SIZE} ${BOARD_DIR}/scripts/chip-create-nand-images.sh "${BASE_DIR}"
+	${BOARD_DIR}/scripts/chip-create-nand-images.sh "${BASE_DIR}"
 else
 	echo "No nand type selected. Not generating sparse image"
 fi
