0d2a8084314c6005da3ef4d49741b511bf5a4e97
Wynter Woods <zerotri@gmail.com>
2017-03-03 11:56:59 -0800

    Allow finer control over what gadget-init-scripts includes

--- gadget/package/gadget-init-scripts/gadget-init-scripts.mk
+++ gadget/package/gadget-init-scripts/gadget-init-scripts.mk
@@ -11,10 +11,30 @@ GADGET_INIT_SCRIPTS_LICENSE = MIT
 
 ####
 
+ifeq ($(strip $(BR2_PACKAGE_GADGET_INIT_SCRIPTS_USB_GADGET)),y)
+define GADGET_INIT_SCRIPTS_INSTALL_USB_GADGET
+	$(INSTALL) -D -m 700 $(@D)/etc/init.d/S55_usb_gadget $(TARGET_DIR)/etc/init.d/
+endef
+GADGET_INIT_SCRIPTS_POST_INSTALL_TARGET_HOOKS += GADGET_INIT_SCRIPTS_INSTALL_USB_GADGET
+endif
+
+ifeq ($(strip $(BR2_PACKAGE_GADGET_INIT_SCRIPTS_DOCKERD)),y)
+define GADGET_INIT_SCRIPTS_INSTALL_DOCKERD
+	$(INSTALL) -D -m 700 $(@D)/etc/init.d/S90_dockerd $(TARGET_DIR)/etc/init.d/
+endef
+GADGET_INIT_SCRIPTS_POST_INSTALL_TARGET_HOOKS += GADGET_INIT_SCRIPTS_INSTALL_DOCKERD
+endif
+
 define GADGET_INIT_SCRIPTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 644 $(@D)/etc/modules $(TARGET_DIR)/etc/
 	$(INSTALL) -D -m 644 $(@D)/etc/securetty $(TARGET_DIR)/etc/
-	$(INSTALL) -D -m 700 $(@D)/etc/init.d/S* $(TARGET_DIR)/etc/init.d/
+	$(INSTALL) -D -m 700 $(@D)/etc/init.d/rcS $(TARGET_DIR)/etc/init.d/
+	$(INSTALL) -D -m 700 $(@D)/etc/init.d/rcK $(TARGET_DIR)/etc/init.d/
+	$(INSTALL) -D -m 700 $(@D)/etc/init.d/S01_setup_ttys $(TARGET_DIR)/etc/init.d/
+	$(INSTALL) -D -m 700 $(@D)/etc/init.d/S02_mounts $(TARGET_DIR)/etc/init.d/
+	$(INSTALL) -D -m 700 $(@D)/etc/init.d/S10_modules $(TARGET_DIR)/etc/init.d/
+	$(INSTALL) -D -m 700 $(@D)/etc/init.d/S20_setup_audio_codec $(TARGET_DIR)/etc/init.d/
+
 endef
 
 $(eval $(generic-package))
