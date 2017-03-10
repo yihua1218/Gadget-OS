bade217efb00229d5821086353fb22149a7984ec
computermouth <computermouth@crunchbangplusplus.org>
2017-03-10 15:34:38 -0800

    Implement auto-configuring network gadget for Windows/Mac/Linux

--- gadget/package/gadget-init-scripts/gadget-init-scripts.mk
+++ gadget/package/gadget-init-scripts/gadget-init-scripts.mk
@@ -13,7 +13,11 @@ GADGET_INIT_SCRIPTS_LICENSE = MIT
 
 ifeq ($(strip $(BR2_PACKAGE_GADGET_INIT_SCRIPTS_USB_GADGET)),y)
 define GADGET_INIT_SCRIPTS_INSTALL_USB_GADGET
-	$(INSTALL) -D -m 700 $(@D)/etc/init.d/S55_usb_gadget $(TARGET_DIR)/etc/init.d/
+	$(INSTALL) -D -m 700 $(@D)/etc/init.d/S34_usb_gadget $(TARGET_DIR)/etc/init.d/
+	$(INSTALL) -d -m 755 $(TARGET_DIR)/etc/connman
+	$(INSTALL) -D -m 644 $(@D)/etc/connman/main.conf $(TARGET_DIR)/etc/connman/
+	$(INSTALL) -D -m 644 $(@D)/etc/dnsmasq.conf $(TARGET_DIR)/etc/
+	$(INSTALL) -D -m 644 $(@D)/etc/network/interfaces $(TARGET_DIR)/etc/network/
 endef
 GADGET_INIT_SCRIPTS_POST_INSTALL_TARGET_HOOKS += GADGET_INIT_SCRIPTS_INSTALL_USB_GADGET
 endif
@@ -34,7 +38,6 @@ define GADGET_INIT_SCRIPTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 700 $(@D)/etc/init.d/S02_mounts $(TARGET_DIR)/etc/init.d/
 	$(INSTALL) -D -m 700 $(@D)/etc/init.d/S10_modules $(TARGET_DIR)/etc/init.d/
 	$(INSTALL) -D -m 700 $(@D)/etc/init.d/S20_setup_audio_codec $(TARGET_DIR)/etc/init.d/
-
 endef
 
 $(eval $(generic-package))
