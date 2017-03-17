5a12b5080f4c08838885c43fac2a4d9d1703e3b9
Alex Kaplan <kaplan2539@gmail.com>
2017-03-17 12:27:48 -0700

    gadgetosd: added /etc/init.d/S99_gadgetosd

--- gadget/package/gadgetosd/gadgetosd.mk
+++ gadget/package/gadgetosd/gadgetosd.mk
@@ -17,6 +17,8 @@ define GADGETOSD_BUILD_CMDS
 endef
 
 define GADGETOSD_INSTALL_TARGET_CMDS
+    $(INSTALL) -m 0755 -D $(GADGETOSD_PKGDIR)/S99_gadgetosd \
+        $(TARGET_DIR)/etc/init.d/S99_gadgetosd
 	$(INSTALL) -D -m 0755 $(@D)/gadgetosd $(TARGET_DIR)/usr/bin
 	$(INSTALL) -D -m 0755 $(@D)/gadget $(TARGET_DIR)/usr/bin
 endef
