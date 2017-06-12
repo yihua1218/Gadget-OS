6805faabbd00e41526b5b261b4ed242c0b328e7b
Alex Kaplan <kaplan2539@gmail.com>
2017-06-12 12:01:16 -0700

    chip-nand-scripts: use new github repo

--- gadget/package/chip-nand-scripts/chip-nand-scripts.mk
+++ gadget/package/chip-nand-scripts/chip-nand-scripts.mk
@@ -6,12 +6,13 @@
 
 CHIP_NAND_SCRIPTS_VERSION = master
 CHIP_NAND_SCRIPTS_REPO_NAME = CHIP-nand-scripts
-CHIP_NAND_SCRIPTS_SITE = https://gitlab.com/kaplan2539/$(CHIP_NAND_SCRIPTS_REPO_NAME)
+CHIP_NAND_SCRIPTS_SITE = https://github.com/nextthingco/$(CHIP_NAND_SCRIPTS_REPO_NAME)
 CHIP_NAND_SCRIPTS_SITE_METHOD = git
 CHIP_NAND_SCRIPTS_DEPENDENCIES = mtd uboot-tools android-tools
 
 define HOST_CHIP_NAND_SCRIPTS_INSTALL_CMDS
 	$(INSTALL) -D -m 0755 $(HOST_CHIP_NAND_SCRIPTS_DIR)/mk_buildroot_images $(HOST_DIR)/usr/bin/mk_buildroot_images
+	$(INSTALL) -D -m 0755 $(HOST_CHIP_NAND_SCRIPTS_DIR)/mk_gadget_images $(HOST_DIR)/usr/bin/mk_gadget_images
 	$(INSTALL) -D -m 0755 $(HOST_CHIP_NAND_SCRIPTS_DIR)/mk_spl_image $(HOST_DIR)/usr/bin/mk_spl_image
 	$(INSTALL) -D -m 0755 $(HOST_CHIP_NAND_SCRIPTS_DIR)/mk_ubi_image $(HOST_DIR)/usr/bin/mk_ubi_image
 	$(INSTALL) -D -m 0755 $(HOST_CHIP_NAND_SCRIPTS_DIR)/mk_ubifs_image $(HOST_DIR)/usr/bin/mk_ubifs_image
