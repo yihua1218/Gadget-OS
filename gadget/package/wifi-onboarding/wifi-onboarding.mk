ab335aeed1fa695c2f63139240f428ad13794f3b
Alex Kaplan <alex@nextthing.co>
2017-08-04 14:50:57 -0700

    added wifi-onboarding

---
+++ gadget/package/wifi-onboarding/wifi-onboarding.mk
@@ -0,0 +1,34 @@
+################################################################################
+#
+# wifi-onboarding
+#
+################################################################################
+
+WIFI_ONBOARDING_VERSION = unstable
+WIFI_ONBOARDING_SITE = https://github.com/nextthingco/wifi-onboarding
+WIFI_ONBOARDING_SITE_METHOD = git
+
+WIFI_ONBOARDING_DEPENDENCIES = host-go
+
+WIFI_ONBOARDING_GOPATH = "$(@D)/gopath"
+WIFI_ONBOARDING_MAKE_ENV = $(HOST_GO_TARGET_ENV) \
+	GOBIN="$(@D)/bin" \
+	GOPATH="$(WIFI_ONBOARDING_GOPATH)"\
+	$(TARGET_MAKE_ENV)
+
+define WIFI_ONBOARDING_BUILD_CMDS
+        $(MAKE) clean
+        $(WIFI_ONBOARDING_MAKE_ENV) $(MAKE) -C $(@D) get
+        $(WIFI_ONBOARDING_MAKE_ENV) GIT_COMMIT=$(WIFI_ONBOARDING_VERSION) $(MAKE) -C $(@D) wifi-onboarding
+endef
+
+define WIFI_ONBOARDING_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/build/linux_arm/wifi-onboarding $(TARGET_DIR)/usr/bin/wifi-onboarding
+endef
+
+#define WIFI_ONBOARDING_INSTALL_INIT_SYSV
+#        $(INSTALL) -m 755 -D package/gadget-initc/S91_gadget_initc \
+#                $(TARGET_DIR)/etc/init.d/S91_gadget_initc
+#endef
+
+$(eval $(generic-package))
