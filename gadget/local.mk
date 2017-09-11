3c92c1971f3e126b6f37c097dce4a6f3d591a9b8
computermouth <computermouth@crunchbangplusplus.org>
2017-09-11 16:13:31 -0700

    Fix builds against latest BR, wifi-onboarding should build and work now as well

--- gadget/local.mk
+++
@@ -1,2 +0,0 @@
-#LINUX_OVERRIDE_SRCDIR = /local/linux
-WIFI_ONBOARDING_OVERRIDE_SRCDIR = /local/wifi-onboarding
--- gadget/package/wifi-onboarding/wifi-onboarding.mk
+++ gadget/package/wifi-onboarding/wifi-onboarding.mk
@@ -10,29 +10,33 @@ WIFI_ONBOARDING_SITE_METHOD = git
 
 WIFI_ONBOARDING_DEPENDENCIES = host-go
 
+WIFI_ONBOARDING_ASSET_PATH=/usr/lib/wifi-onboarding
+
 WIFI_ONBOARDING_GOPATH = "$(@D)/gopath"
 WIFI_ONBOARDING_MAKE_ENV = $(HOST_GO_TARGET_ENV) \
 	GOBIN="$(@D)/bin" \
-	GOPATH="$(WIFI_ONBOARDING_GOPATH)"\
+	GOPATH="$(WIFI_ONBOARDING_GOPATH)" \
+	WIFI_ONBOARDING_VIEW_LOCATION="$(WIFI_ONBOARDING_ASSET_PATH)/view/*" \
+	WIFI_ONBOARDING_STATIC_LOCATION="$(WIFI_ONBOARDING_ASSET_PATH)/static" \
 	$(TARGET_MAKE_ENV)
 
 define WIFI_ONBOARDING_BUILD_CMDS
         $(MAKE) clean
         $(WIFI_ONBOARDING_MAKE_ENV) $(MAKE) -C $(@D) get
-        $(WIFI_ONBOARDING_MAKE_ENV) GIT_COMMIT=$(WIFI_ONBOARDING_VERSION) $(MAKE) -C $(@D) wifi-onboarding
+        $(WIFI_ONBOARDING_MAKE_ENV) GIT_COMMIT=$(WIFI_ONBOARDING_VERSION) $(MAKE) -C $(@D)
 endef
 
 define WIFI_ONBOARDING_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/build/linux_arm/wifi-onboarding $(TARGET_DIR)/usr/bin/wifi-onboarding
-	$(INSTALL) -d -m 0644 $(TARGET_DIR)/usr/lib/wifi-onboarding/view
-	$(INSTALL) -d -m 0644 $(TARGET_DIR)/usr/lib/wifi-onboarding/static
-	$(INSTALL) -D -m 0644 $(@D)/view/*   $(TARGET_DIR)/usr/lib/wifi-onboarding/view
-	$(INSTALL) -D -m 0644 $(@D)/static/* $(TARGET_DIR)/usr/lib/wifi-onboarding/static
+	$(INSTALL) -d -m 0644 $(TARGET_DIR)$(WIFI_ONBOARDING_ASSET_PATH)/view
+	$(INSTALL) -d -m 0644 $(TARGET_DIR)$(WIFI_ONBOARDING_ASSET_PATH)/static
+	$(INSTALL) -D -m 0644 $(@D)/view/*   $(TARGET_DIR)$(WIFI_ONBOARDING_ASSET_PATH)/view
+	$(INSTALL) -D -m 0644 $(@D)/static/* $(TARGET_DIR)$(WIFI_ONBOARDING_ASSET_PATH)/static
 endef
 
-#define WIFI_ONBOARDING_INSTALL_INIT_SYSV
-#        $(INSTALL) -m 755 -D package/gadget-initc/S91_gadget_initc \
-#                $(TARGET_DIR)/etc/init.d/S91_gadget_initc
-#endef
+define WIFI_ONBOARDING_INSTALL_INIT_SYSV
+    $(INSTALL) -D -m 755 $(BR2_EXTERNAL_GADGETOS_PATH)/package/wifi-onboarding/S50_wifi_onboarding \
+		$(TARGET_DIR)/etc/init.d/S50_wifi_onboarding
+endef
 
 $(eval $(generic-package))
